c0dd6fa4cfd423abf78012b0b1b8f401f79f807f
Paval Miatlitski <paval.escoba@gmail.com>
2019-02-05 15:11:48 +0300

    feat: end of lists and conditions module

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { name: 'Max', age: 28, id: '112'},
+      { name: 'Manu', age: 29, id: '122'},
+      { name: 'Stephanie', age: 26, id: '212'}
     ],
     otherState: 'some other value',
     showPersons: false
@@ -15,31 +15,38 @@ class App extends Component {
   switchNameHandler = (newName) => {
     this.setState( {
       persons: [
-        { name: newName, age: 33 },
         { name: 'Manu', age: 29  },
         { name: 'Stephanie', age: 27 }
       ]
     } )
   };
-  nameChangedHanler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Paval', age: 33 },
-        { name: event.target.value, age: 29  },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHanler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+    // old way - const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons: persons})
   };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
   }
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
   render() {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1px solid blue',
+      border: '1px solid indianred',
       padding: '0.5rem',
       cursor: 'pointer'
     }
@@ -48,10 +55,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person) => {
+          {this.state.persons.map((person, index) => {
             return <Person 
-                    name={person.name}
-                    age={person.age } />
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(e)=>this.nameChangedHanler(e, person.id)} />
           })}
        </div> 
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,8 @@ import './Person.css'
 const person = ( props ) => {
     return (
         <div className="Person">
-            <p onClick={props.clicking}>I'm {props.name} and I am {props.age} years old! {props.children}</p>
-            <input type="text" onChange={props.changing} value={props.name}></input>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old! {props.children}</p>
+           <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     )
 };
