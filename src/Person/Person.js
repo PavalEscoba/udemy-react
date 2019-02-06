99994a79926a33663d1d95ce334494e16ef7bfa5
Paval Miatlitski <paval.escoba@gmail.com>
2019-02-06 15:21:41 +0300

    feat: use ErrorBoundary functionality

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './Person.module.css'
 
 const person = ( props ) => {
+    const  random = Math.random();
+    if (random > 0.7){
+        throw new Error('smth went wrong');
+    }
     return (
         <div className={styles.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old! {props.children}</p>
