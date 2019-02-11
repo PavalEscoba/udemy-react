a5e9d3db292494904e9f34ba603dc17129c85372
Paval Miatlitski <paval.escoba@gmail.com>
2019-02-12 01:31:49 +0300

    feat:burger controls is done

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BuildControl from "./BuildControl/BuildControl";
+import classes from "./BuildControls.module.css";
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    { controls.map(ctrl => (
+        <BuildControl id={ctrl.label} key={ctrl.label} type={ctrl.label} />))}
+  </div>
+);
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary/Auxiliary";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component{
+  // constructor(propr){
+  //   super(prop);
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients:{
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+  addIngredientHandler = (type) => {
+    const oldCount  = this.state.ingredients[type];
+    const updatedCounted = oldCount + 1;
+  }
+  removeIngredientHandlee = (type) => {
+
+  }
   render(){
     return (
       <Aux>
-        <Burger />
-        <div> Build Controls (add and remove ingredients)</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls />
       </Aux>
     );
   }
